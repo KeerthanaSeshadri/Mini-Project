b7dc78689a71e4c951cd8b03e95c7a248759f8e8
KeerthanaSeshadri <keerthanaseshadri2006@gmail.com>
2024-06-04 14:37:48 +0530

    Update cricketScoreboard.c

--- cricketScoreboard.c
+++ cricketScoreboard.c
@@ -4,7 +4,7 @@
 struct batsman
  {
    char name[25];
-   int runs,score,balls,totalRuns,totalBalls,ones,twos,fours,sixes;
+   int runs,score,balls,toruns,tobal,ones,twos,threes,fours,sixes;
    int max_six,max_run,max_four;
    float str;
 
@@ -14,7 +14,7 @@ struct batsman
 struct bowler
  {
    char name[25];
-   int runsGiven,wicketsTaken,oversBowled;
+   int runsgv,wkttkn,overs;
    int max_w;
    float econ;
  }pl2[100],pl4;
@@ -40,6 +40,10 @@ int main()
 
        printf("Enter the number of twos scored by player%d:\n ",i+1);
        scanf("%d",&pl1[i].twos);
+
+
+       printf("Enter the number of threes scored by player%d:\n ",i+1);
+       scanf("%d",&pl1[i].threes);
 
 
        printf("Enter the number of fours scored by player%d:\n ",i+1);
@@ -71,17 +75,19 @@ int main()
 
 
        printf("Enter the runs given by the bowler%d:\n ",i+1);
-       scanf("%d",&pl2[i].runsGiven);
+       scanf("%d",&pl2[i].runsgv);
 
 
        printf("Enter the overs bowled by the bowler%d:\n",i+1);
-       scanf("%d",&pl2[i].oversBowled);
+       scanf("%d",&pl2[i].overs);
 
 
        printf("Enter the wickets taken by the bowler%d\n",i+1);
-       scanf("%d",&pl2[i].wicketsTaken);
+       scanf("%d",&pl2[i].wkttkn);
 
    }
+
+   printf("Thank you all details are recorded\n");
 
 
    do
@@ -104,7 +110,7 @@ int main()
               printf("===========================================================================\n");
 
 
-              pl1[plno].runs=(1*pl1[plno].ones)+(2*pl1[plno].twos)+(4*pl1[plno].fours)+(6*pl1[plno].sixes);
+              pl1[plno].runs=(1*pl1[plno].ones)+(2*pl1[plno].twos)+(3*pl1[plno].threes)+(4*pl1[plno].fours)+(6*pl1[plno].sixes);
               pl1[plno].str=(pl1[plno].runs*100.00)/pl1[plno].balls;
               printf(" %-15s %-14d %-13d %-11d %-11d %-9.2f\n\n",pl1[plno].name,pl1[plno].runs,pl1[plno].balls,pl1[plno].fours,pl1[plno].sixes,pl1[plno].str);
 
@@ -122,8 +128,8 @@ int main()
               printf("=================================================================\n");
 
                for(i=0;i<n;i++)
-               {   pl2[plno].econ=pl2[plno].runsGiven/pl2[plno].oversBowled;
-                   printf(" %-15s %-14d %-13d %-11d %-11.2f\n\n",pl2[plno].name,pl2[plno].oversBowled,pl2[plno].runsGiven,pl2[plno].wicketsTaken,pl2[plno].econ);
+               {   pl2[plno].econ=pl2[plno].runsgv/pl2[plno].overs;
+                   printf(" %-15s %-14d %-13d %-11d %-11.2f\n\n",pl2[plno].name,pl2[plno].overs,pl2[plno].runsgv,pl2[plno].wkttkn,pl2[plno].econ);
                }
 
              break;
@@ -136,20 +142,20 @@ int main()
 
               for(i=0;i<1;i++)
                 {
-                    pl1[i].runs=(1*pl1[i].ones)+(2*pl1[i].twos)+(4*pl1[i].fours)+(6*pl1[i].sixes);
-                    pl3.totalRuns+=pl1[i].runs;
+                    pl1[i].runs=(1*pl1[i].ones)+(2*pl1[i].twos)+(3*pl1[i].threes)+(4*pl1[i].fours)+(6*pl1[i].sixes);
+                    pl3.toruns+=pl1[i].runs;
                     pl1[i].str=(pl1[i].runs*100.00)/pl1[i].balls;
                     printf(" %-15s %-14d %-13d %-11d %-11d %-9.2f\n\n",pl1[i].name,pl1[i].runs,pl1[i].balls,pl1[i].fours,pl1[i].sixes,pl1[i].str);
                 }
-                printf("TOTAL RUNS:%d\n\n",pl3.totalRuns);
+                printf("TOTAL RUNS:%d\n\n",pl3.toruns);
               printf("\n\n");
               printf("=================================================================\n");
               printf(" Bowler        overs           runs        wicket       economy\n");
               printf("=================================================================\n");
 
                for(i=0;i<n;i++)
-               {   pl2[i].econ=pl2[i].runsGiven/pl2[i].oversBowled;
-                   printf(" %-15s %-14d %-13d %-11d %-11.2f\n\n\n",pl2[i].name,pl2[i].oversBowled,pl2[i].runsGiven,pl2[i].wicketsTaken,pl2[i].econ);
+               {   pl2[i].econ=pl2[i].runsgv/pl2[i].overs;
+                   printf(" %-15s %-14d %-13d %-11d %-11.2f\n\n\n",pl2[i].name,pl2[i].overs,pl2[i].runsgv,pl2[i].wkttkn,pl2[i].econ);
                }
 
 
@@ -159,7 +165,7 @@ int main()
        
                 for(i=0;i<m;i++)
                   { 
-                     pl1[i].runs=(1*pl1[i].ones)+(2*pl1[i].twos)+(4*pl1[i].fours)+(6*pl1[i].sixes);
+                     pl1[i].runs=(1*pl1[i].ones)+(2*pl1[i].twos)+(3*pl1[i].threes)+(4*pl1[i].fours)+(6*pl1[i].sixes);
                      if(pl3.max_run<pl1[i].runs)
                         {
                           pl3.max_run=pl1[i].runs;
@@ -176,9 +182,9 @@ int main()
                         pl3.max_four=pl1[i].fours;
                        }
  
-                     if(pl4.max_w<pl2[i].wicketsTaken)
+                     if(pl4.max_w<pl2[i].wkttkn)
                       {
-                      pl4.max_w=pl2[i].wicketsTaken;
+                      pl4.max_w=pl2[i].wkttkn;
                       }
                   }
               printf("Highest runs scored by the batsman:%d\n",pl3.max_run);
